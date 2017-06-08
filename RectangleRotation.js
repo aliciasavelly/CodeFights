5f6b4975f9678e3008aa832d56eb2aadb1cf44e0
aliciasavelly <alicia.savelly@gmail.com>
2017-06-08 14:34:27 -0700

    complete algorithm (passes tests)

--- RectangleRotation.js
+++ RectangleRotation.js
@@ -1,24 +1,52 @@
 function rectangleRotation(a, b) {
-//     // return ((a - 1) * (b - 1) + (a - 2) * (b - 2));
+  var middots = Math.floor(a / 1.4 / 2);
+  var amidlen = middots * 2 + 1;
+  var bmidlen = 1;
+  var current1 = [middots, middots];
+  var current2 = [middots, middots];
+  var notFoundBMidLen = true;
 
-//     var amidlen = Math.floor(a / 1.4 / 2) * 2 + 1;
-//     var bmidlen = Math.floor(b / 1.4 / 2) * 2 + 1;
-//     var middots = amidlen * bmidlen;
-//     console.log(middots);
+  while (notFoundBMidLen) {
+    current1[0] -= 1;
+    current1[1] += 1;
+    current2[0] += 1;
+    current2[1] -= 1;
 
-
-  var sqr = function(x) {
-    return x * x;
-  }
-
-  var answer = 0;
-  for (var x = -a - b; x <= a + b; x++) {
-    for (var y = -b; y <= b; y++) {
-      if (2 * sqr(x - y) <= sqr(a) && 2 * sqr(x + y) <= sqr(b)) {
-        answer++;
-      }
+    var dist = Math.sqrt( Math.pow((current2[0] - current1[0]), 2) + Math.pow((current2[1] - current1[1]), 2) );
+    if (dist > b) {
+      notFoundBMidLen = false;
+      current1[0] += 1;
+      current1[1] -= 1;
+      current2[0] -= 1;
+      current2[1] += 1;
+    } else {
+      bmidlen += 2;
     }
   }
 
-  return answer;
+  var testPoint = [current1[0] - 1, current1[1]];
+  var testPoint2 = [testPoint[1], testPoint[0]];
+  var dist = Math.sqrt( Math.pow((testPoint2[0] - testPoint[0]), 2) + Math.pow((testPoint2[1] - testPoint[1]), 2) );
+  if (dist > b) {
+    var bofflen = bmidlen - 1;
+    var aofflen = amidlen - 1;
+  } else {
+    var bofflen = bmidlen + 1;
+    var aofflen = amidlen + 1;
+  }
+
+  var testPoint = [current1[0] + 1, current1[1]];
+  var testPoint2 = [testPoint[1] * -1, testPoint[0] * -1];
+  var dist = Math.sqrt( Math.pow((testPoint2[0] - testPoint[0]), 2) + Math.pow((testPoint2[1] - testPoint[1]), 2) );
+
+  if (dist < a) {
+    aofflen = amidlen + 1;
+  }
+  
+  return (bmidlen * amidlen) + (bofflen * aofflen);
 }
+
+
+
+console.log(rectangleRotation(6, 4));
+console.log(rectangleRotation(30, 2));
