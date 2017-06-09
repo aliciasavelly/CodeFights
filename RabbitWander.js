f20ec013463c4a965468b0d04859a765a8f6b4a3
aliciasavelly <alicia.savelly@gmail.com>
2017-06-09 00:41:39 -0700

    write out rabbit wander

---
+++ RabbitWander.js
@@ -0,0 +1,9 @@
+function rabbitWander(matrix) {
+  
+}
+
+let matrix = [[5, 7, 8, 6, 3],
+              [0, 0, 7, 0, 4],
+              [4, 6, 3, 4, 9],
+              [3, 1, 0, 5, 8]]
+console.log(rabbitWander(matrix) == 27);
