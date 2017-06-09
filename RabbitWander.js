58fc08a578d51c8a83948dbe89fa141866706ee9
aliciasavelly <alicia.savelly@gmail.com>
2017-06-09 00:53:39 -0700

    finish find middle pos for rabbit wander

--- RabbitWander.js
+++ RabbitWander.js
@@ -1,9 +1,61 @@
 function rabbitWander(matrix) {
-  
+  let middlePos = findMiddlePos(matrix);
+  console.log(middlePos);
 }
 
-let matrix = [[5, 7, 8, 6, 3],
-              [0, 0, 7, 0, 4],
-              [4, 6, 3, 4, 9],
-              [3, 1, 0, 5, 8]]
-console.log(rabbitWander(matrix) == 27);
+function findMiddlePos(matrix) {
+  var rows = [];
+  var cols = [];
+  if (matrix.length % 2 == 0) {
+    rows.push(matrix.length / 2);
+    rows.push(matrix.length / 2 - 1);
+  } else {
+    rows.push(Math.floor(matrix.length / 2));
+  }
+  if (matrix[0].length % 2 == 0) {
+    cols.push(matrix[0].length / 2);
+    cols.push(matrix[0].length / 2 - 1);
+  } else {
+    cols.push(Math.floor(matrix[0].length / 2));
+  }
+
+  if (rows.length == 1) {
+    if (cols.length == 1) {
+      return [[rows[0], cols[0]]];
+    } else {
+      return [[rows[0], cols[0]], [rows[0], cols[1]]];
+    }
+  } else {
+    if (cols.length == 1) {
+      return [[rows[0], cols[0]], [rows[1], cols[0]]];
+    } else {
+      return [[rows[0], cols[0]], [rows[0], cols[1]], [rows[1], cols[0]], [rows[1], cols[1]]];
+    }
+  }
+}
+
+let m1 = [[5, 7, 8, 6, 3],
+          [0, 0, 7, 0, 4],
+          [4, 6, 3, 4, 9],
+          [3, 1, 0, 5, 8]]
+console.log(rabbitWander(m1) == 27);
+
+let m2 = [[5, 7, 8, 6],
+          [0, 0, 7, 0],
+          [4, 6, 3, 4],
+          [3, 1, 0, 5]]
+console.log(rabbitWander(m2) == 27);
+
+let m3 = [[5, 7, 8, 6, 3],
+          [0, 0, 7, 0, 4],
+          [4, 6, 3, 4, 9],
+          [3, 1, 0, 5, 8],
+          [7, 0, 1, 2, 7]]
+console.log(rabbitWander(m3) == 27);
+
+let m4 = [[5, 7, 8, 6],
+          [0, 0, 7, 0],
+          [4, 6, 3, 4],
+          [3, 1, 0, 5],
+          [2, 0, 1, 3]]
+console.log(rabbitWander(m4) == 27);
