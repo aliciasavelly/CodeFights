82cd3e570af1fafc57d61afb47f8e2f63b2dcaa3
aliciasavelly <alicia.savelly@gmail.com>
2017-06-10 14:15:41 -0700

    work on solution

--- FindFirstSubstringOccurance.js
+++ FindFirstSubstringOccurance.js
@@ -1,33 +1,46 @@
 function findFirstSubstringOccurrence(s, x) {
-    var similar = "";
-    var startPos = -1;
+  // for (var i = 0; i < (s.length - x.length + 1); i++) {
+  //   for (var j = 0; j < x.length && s[j + i] == x[j]; j++) {
+  //   }
+  //
+  //   if (j == x.length) {
+  //     return i;
+  //   }
+  // }
+  //
+  // return -1;
 
-    var j = 0;
-    for (var i = 0; i < s.length; i++) {
-        if (s[i] == x[j]) {
-            similar += x[j];
-            if (j == 0) {
-                startPos = i;
-            }
-            if (similar == x) {
-                // console.log(similar);
-                return startPos;
-            }
-            j += 1;
-        } else {
-            if (startPos > -1) {
-                // console.log(i);
-                // console.log(j);
-                // console.log(startPos);
-                // console.log(similar);
-                startPos = -1;
-                similar = "";
-                i = i - j;
-                j = 0;
-                // console.log(i);
-            }
-        }
-    }
+  return (s).indexOf(x);
 
-    return -1;
+  // var similar = "";
+  // var startPos = -1;
+  //
+  // var j = 0;
+  // for (var i = 0; i < s.length; i++) {
+  //     if (s[i] == x[j]) {
+  //         similar += x[j];
+  //         if (j == 0) {
+  //             startPos = i;
+  //         }
+  //         if (similar == x) {
+  //             // console.log(similar);
+  //             return startPos;
+  //         }
+  //         j += 1;
+  //     } else {
+  //         if (startPos > -1) {
+  //             // console.log(i);
+  //             // console.log(j);
+  //             // console.log(startPos);
+  //             // console.log(similar);
+  //             startPos = -1;
+  //             similar = "";
+  //             i = i - j;
+  //             j = 0;
+  //             // console.log(i);
+  //         }
+  //     }
+  // }
+  //
+  // return -1;
 }
