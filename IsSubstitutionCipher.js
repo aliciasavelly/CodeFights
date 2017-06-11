260fa751f981e321e2e56e656d768e4b76749554
aliciasavelly <alicia.savelly@gmail.com>
2017-06-10 21:26:58 -0700

    add directions

--- IsSubstitutionCipher.js
+++ IsSubstitutionCipher.js
@@ -19,3 +19,42 @@ function checkHash(string1, string2) {
         }
     }
 }
+
+/*
+
+A ciphertext alphabet is obtained from the plaintext alphabet by means of rearranging some characters. For example "bacdef...xyz" will be a simple ciphertext alphabet where a and b are rearranged.
+
+A substitution cipher is a method of encoding where each letter of the plaintext alphabet is replaced with the corresponding (i.e. having the same index) letter of some ciphertext alphabet.
+
+Given two strings, check whether it is possible to obtain them from each other using some (possibly, different) substitution ciphers.
+
+Example
+
+For string1 = "aacb" and string2 = "aabc", the output should be
+isSubstitutionCipher(string1, string2) = true.
+
+Any ciphertext alphabet that starts with acb... would make this transformation possible.
+
+For string1 = "aa" and string2 = "bc", the output should be
+isSubstitutionCipher(string1, string2) = false.
+
+Input/Output
+
+[time limit] 4000ms (js)
+[input] string string1
+
+A string consisting of lowercase English characters.
+
+Guaranteed constraints:
+1 ≤ string1.length ≤ 10.
+
+[input] string string2
+
+A string consisting of lowercase English characters of the same length as string1.
+
+Guaranteed constraints:
+string2.length = string1.length.
+
+[output] boolean
+
+*/
