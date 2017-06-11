e8bd38a9c8e712b97db33fd98b92f2bfba1a47fd
aliciasavelly <alicia.savelly@gmail.com>
2017-06-11 16:13:14 -0700

    add instructions

--- RectangleRotation.js
+++ RectangleRotation.js
@@ -56,9 +56,13 @@ console.log(rectangleRotation(16, 20) == 333);
 
 /*
 
-A rectangle with sides equal to even integers a and b is drawn on the Cartesian plane. Its center (the intersection point of its diagonals) coincides with the point (0, 0), but the sides of the rectangle are not parallel to the axes; instead, they are forming 45 degree angles with the axes.
+A rectangle with sides equal to even integers a and b is drawn on the Cartesian
+plane. Its center (the intersection point of its diagonals) coincides with the
+point (0, 0), but the sides of the rectangle are not parallel to the axes;
+instead, they are forming 45 degree angles with the axes.
 
-How many points with integer coordinates are located inside the given rectangle (including on its sides)?
+How many points with integer coordinates are located inside the given rectangle
+(including on its sides)?
 
 Example
 
