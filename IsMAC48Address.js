219ff4c76acfde77f16b6d9090713d72747ba6dd
aliciasavelly <alicia.savelly@gmail.com>
2017-06-12 14:01:18 -0700

    clean up code

--- IsMAC48Address.js
+++ IsMAC48Address.js
@@ -8,7 +8,8 @@ function isMAC48Address(inputString) {
             if ((i + 1) % 3 != 0) {
                 return false;
             }
-        } else if ( !(inputString[i].charCodeAt(0) >= 65 && inputString[i].charCodeAt(0) <= 70) && !(inputString[i].charCodeAt(0) >= 48 && inputString[i].charCodeAt(0) <= 57)) {
+        } else if ( !(inputString[i].charCodeAt(0) >= 65 && inputString[i].charCodeAt(0) <= 70) &&
+                    !(inputString[i].charCodeAt(0) >= 48 && inputString[i].charCodeAt(0) <= 57)) {
             return false;
         }
     }
@@ -24,7 +25,7 @@ The standard (IEEE 802) format for printing MAC-48 addresses in human-friendly f
 
 Your task is to check by given string inputString whether it corresponds to MAC-48 address or not.
 
-Example
+Example:
 
 For inputString = "00-1B-63-84-45-E6", the output should be
 isMAC48Address(inputString) = true;
