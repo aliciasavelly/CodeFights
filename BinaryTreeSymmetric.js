503eacd0eafbcfd6939792cb0d7eef1b97dd6245
aliciasavelly <alicia.savelly@gmail.com>
2017-06-13 19:22:56 -0700

    add recursive solution

--- BinaryTreeSymmetric.js
+++ BinaryTreeSymmetric.js
@@ -4,7 +4,37 @@ function Tree(x) {
   this.right = null;
 }
 
-root = Tree(1)
+/*
+Recursive Solution
+*/
+function isTreeSymmetric(t) {
+    if (t == undefined) {
+        return true;
+    }
+
+    return areTreesMirror(t.left, t.right);
+}
+
+function areTreesMirror(t1, t2) {
+    if (t1 == undefined && t2 == undefined) {
+        return true;
+    }
+    if (t1 == undefined || t2 == undefined) {
+        return false;
+    }
+    if (t1.value != t2.value) {
+        return false;
+    }
+
+    if (areTreesMirror(t1.left, t2.right) == false) {
+        return false;
+    }
+
+    return areTreesMirror(t1.right, t2.left);
+}
+
+
+let root = Tree(1)
 root.left = Node(2)
 root.right = Node(2)
 root.left.left = Node(3)
