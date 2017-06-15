12919581c35f262dc8aa433a0126f83996a0008d
aliciasavelly <alicia.savelly@gmail.com>
2017-06-15 16:54:14 -0700

    refactor

--- IsOneSwapEnough.js
+++ IsOneSwapEnough.js
@@ -1,26 +1,26 @@
 function isOneSwapEnough(inputString) {
-    if (isPalindrome(inputString)) {
+  if (isPalindrome(inputString)) {
+    return true;
+  }
+
+  for (let i = 0; i < inputString.length - 1; i++) {
+    if (inputString[i] != inputString[i + 1]) {
+      let mixed = inputString.slice(0, i) + inputString[i + 1] + inputString[i] + inputString.slice(i + 2, inputString.length);
+      if (isPalindrome(mixed)) {
         return true;
+      }
     }
+  }
 
-    for (var i = 0; i < inputString.length - 1; i++) {
-        if (inputString[i] != inputString[i + 1]) {
-            var mixed = inputString.slice(0, i) + inputString[i + 1] + inputString[i] + inputString.slice(i + 2, inputString.length);
-            if (isPalindrome(mixed)) {
-                return true;
-            }
-        }
-    }
-
-    return false;
+  return false;
 }
 
 function isPalindrome(string) {
-    for (var i = 0; i <= string.length / 2; i++) {
-        if (string[i] != string[string.length - 1 - i]) {
-            return false;
-        }
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string[i] != string[string.length - 1 - i]) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
