abd1909af89280493efbc2d0d2ad151e75b0ba64
aliciasavelly <alicia.savelly@gmail.com>
2017-06-15 13:58:34 -0700

    clean up code for anagrams

--- CreateAnagram.js
+++ CreateAnagram.js
@@ -1,27 +1,3 @@
-function createAnagram(s, t) {
-    var sHash = {};
-    for (var i = 0; i < s.length; i++) {
-        if (sHash[s[i]] == undefined) {
-            sHash[s[i]] = 1;
-        } else {
-            sHash[s[i]] += 1;
-        }
-    }
-
-    for (var j = 0; j < t.length; j++) {
-        if (sHash[t[j]] != undefined && sHash[t[j]] > 0) {
-            sHash[t[j]] -= 1;
-        }
-    }
-
-    var result = 0;
-    for (var key in sHash) {
-        result += sHash[key];
-    }
-
-    return result;
-}
-
 /*
 You are given two strings s and t of the same length, consisting of uppercase
 English letters. Your task is to find the minimum number of "replacement
@@ -53,3 +29,27 @@ t.length = s.length.
 The minimum number of replacement operations needed to get an anagram of the
 string t from the string s.
 */
+
+function createAnagram(s, t) {
+  let sHash = {};
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    sHash[s[i]] ? sHash[s[i]] += 1 : sHash[s[i]] = 1;
+  }
+
+  for (let j = 0; j < t.length; j++) {
+    if (sHash[t[j]] > 0) {
+      sHash[t[j]] -= 1;
+    }
+  }
+
+  for (let key in sHash) {
+    result += sHash[key];
+  }
+
+  return result;
+}
+
+console.log(createAnagram("AABAA", "BBAAA"));
+console.log(createAnagram("OVGHK", "RPGUC"));
