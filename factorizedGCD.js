3887753dc73da87e63ec4f35e4fbe26cf4ba57fb
aliciasavelly <alicia.savelly@gmail.com>
2017-06-15 21:42:03 -0700

    clean up code

--- factorizedGCD.js
+++ factorizedGCD.js
@@ -1,26 +1,23 @@
 function factorizedGCD(a, b) {
-    var first = 1;
-    var second = 1;
+  let first = 1;
+  let second = 1;
 
-    for (var i = 0; i < a.length; i++) {
-        first *= a[i];
-    }
-    for (var i = 0; i < b.length; i++) {
-        second *= b[i];
-    }
+  for (let i = 0; i < a.length; i++) {
+    first *= a[i];
+  }
+  for (let i = 0; i < b.length; i++) {
+    second *= b[i];
+  }
 
-    return findGCD(first, second);
+  return findGCD(first, second);
 }
 
 function findGCD(first, second) {
-    for (var i = Math.min(first, second); i >= 2; i--) {
-        if ((first / i) % 1 === 0 && (second / i) % 1 === 0) {
-            console.log(i);
-            console.log(first);
-            console.log(second);
-            return i;
-        }
+  for (let i = Math.min(first, second); i >= 2; i--) {
+    if ((first / i) % 1 === 0 && (second / i) % 1 === 0) {
+      return i;
     }
+  }
 
-    return 1;
+  return 1;
 }
