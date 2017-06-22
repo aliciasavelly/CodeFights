a1473c366a1dccdf9611cf77dd42401883eddb2e
aliciasavelly <alicia.savelly@gmail.com>
2017-06-21 18:58:34 -0700

    refactor

--- calendar.js
+++ calendar.js
@@ -40,9 +40,7 @@ function calendar(month, year) {
 function leapYear(year) {
     if (year % 4 != 0) {
         return false;
-    } else if (year % 100 != 0) {
-        return true;
-    } else if (year % 400 != 0) {
+    } else if (year % 100 == 0 && year % 400 != 0) {
         return false;
     }
     return true;
