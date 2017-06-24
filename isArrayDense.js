bc7812fcce2aa18bd7b9414ac3fa52ca60756488
aliciasavelly <alicia.savelly@gmail.com>
2017-06-24 14:51:58 -0700

    clean up code

--- isArrayDense.js
+++ isArrayDense.js
@@ -1,25 +1,25 @@
 function isArrayDense(a) {
-    var min = Infinity;
-    var max = -Infinity;
+  var min = Infinity;
+  var max = -Infinity;
 
-    for (var i = 0; i < a.length; i++) {
-        if (a[i] < min) {
-            min = a[i];
-        }
-        if (a[i] > max) {
-            max = a[i];
-        }
+  for (var i = 0; i < a.length; i++) {
+    if (a[i] < min) {
+      min = a[i];
     }
-
-    if ((max - min) == (a.length - 1)) {
-        var result = true;
-        for (var i = min; i < min + a.length; i++) {
-            if (!a.includes(i)) {
-                result = false;
-            }
-        }
-        return result;
+    if (a[i] > max) {
+      max = a[i];
     }
+  }
 
-    return false;
+  if ((max - min) == (a.length - 1)) {
+    var result = true;
+    for (var i = min; i < min + a.length; i++) {
+      if (!a.includes(i)) {
+        result = false;
+      }
+    }
+    return result;
+  }
+
+  return false;
 }
