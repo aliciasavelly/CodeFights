62010de72b91f5c5de5365f6577a1449880b5936
aliciasavelly <alicia.savelly@gmail.com>
2017-06-24 14:51:38 -0700

    add is array dense

---
+++ isArrayDense.js
@@ -0,0 +1,25 @@
+function isArrayDense(a) {
+    var min = Infinity;
+    var max = -Infinity;
+
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] < min) {
+            min = a[i];
+        }
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+
+    if ((max - min) == (a.length - 1)) {
+        var result = true;
+        for (var i = min; i < min + a.length; i++) {
+            if (!a.includes(i)) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    return false;
+}
