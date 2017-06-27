f6513402e67d6aaa2226f5a1d54afa05e5b4d170
aliciasavelly <alicia.savelly@gmail.com>
2017-06-27 12:19:00 -0700

    add reflect string

---
+++ reflectString.rb
@@ -0,0 +1,14 @@
+def reflectString(inputString)
+  hash = {}
+  result = ""
+
+  97.upto(122) do |i|
+    hash[i.chr()] = (122 - (i - 97)).chr()
+  end
+
+  inputString.each_char do |char|
+    result +=  hash[char]
+  end
+
+  result
+end
