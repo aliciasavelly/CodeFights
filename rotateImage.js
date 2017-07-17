24c45ea4db4beb407cb26da9867b9dd39be878d3
aliciasavelly <alicia.savelly@gmail.com>
2017-07-16 19:10:56 -0700

    clean up code

--- rotateImage.js
+++ rotateImage.js
@@ -1,26 +1,26 @@
 function rotateImage(a) {
-    let current;
-    let next;
+  let current;
+  let next;
 
-    for (let i = 0; i < a.length - 1; i++) {
-        next = a[i][a.length - 1];
-        a[i][a.length - 1] = a[0][i];
-        current = next;
+  for (let i = 0; i < a.length - 1; i++) {
+    next = a[i][a.length - 1];
+    a[i][a.length - 1] = a[0][i];
+    current = next;
 
-        next = a[a.length - 1][a.length - 1 - i];
-        a[a.length - 1][a.length - 1 - i] = current;
-        current = next;
+    next = a[a.length - 1][a.length - 1 - i];
+    a[a.length - 1][a.length - 1 - i] = current;
+    current = next;
 
-        next = a[a.length - 1 - i][0];
-        a[a.length - 1 - i][0] = current;
-        current = next;
+    next = a[a.length - 1 - i][0];
+    a[a.length - 1 - i][0] = current;
+    current = next;
 
 
-        next = a[0][i];
-        a[0][i] = current;
-        current = next;
-        console.log(a);
-    }
+    next = a[0][i];
+    a[0][i] = current;
+    current = next;
+    console.log(a);
+  }
 
-    return a;
+  return a;
 }
