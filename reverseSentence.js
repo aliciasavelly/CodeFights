a83ba8de6d1649b149ea486e59dfe8c194830418
aliciasavelly <alicia.savelly@gmail.com>
2017-08-07 23:11:26 -0700

    write reverse sentence

--- reverseSentence.js
+++ reverseSentence.js
@@ -1,5 +1,12 @@
 function reverseSentence(sentence) {
-  
+  let sentenceArr = sentence.split(" ");
+  let result = [];
+
+  for (let i = sentenceArr.length - 1; i >= 0; i--) {
+    result.push(sentenceArr[i]);
+  }
+
+  return result.join(" ");
 }
 
 console.log(reverseSentence("Man bites dog") == "dog bites Man");
