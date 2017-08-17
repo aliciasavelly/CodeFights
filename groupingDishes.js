9eefbf90aeea12af1d22b5bbdc237b5959696031
aliciasavelly <alicia.savelly@gmail.com>
2017-08-16 22:34:25 -0700

    clean up code

--- groupingDishes.js
+++ groupingDishes.js
@@ -1,26 +1,26 @@
 function groupingDishes(dishes) {
-    let ingredients = {};
+  let ingredients = {};
 
-    for (let i = 0; i < dishes.length; i++) {
-        let currentDish = dishes[i][0];
+  for (let i = 0; i < dishes.length; i++) {
+    let currentDish = dishes[i][0];
 
-        for (let j = 1; j < dishes[i].length; j++) {
-            let currentIngredient = dishes[i][j];
-            if (ingredients[currentIngredient]) {
-                ingredients[currentIngredient].push(currentDish);
-            } else {
-                ingredients[currentIngredient] = [currentDish];
-            }
-        }
+    for (let j = 1; j < dishes[i].length; j++) {
+      let currentIngredient = dishes[i][j];
+      if (ingredients[currentIngredient]) {
+        ingredients[currentIngredient].push(currentDish);
+      } else {
+        ingredients[currentIngredient] = [currentDish];
+      }
     }
+  }
 
-    let result = [];
+  let result = [];
 
-    for (let food in ingredients) {
-        if (ingredients[food].length > 1) {
-            result.push([food].concat(ingredients[food].sort()));
-        }
+  for (let food in ingredients) {
+    if (ingredients[food].length > 1) {
+      result.push([food].concat(ingredients[food].sort()));
     }
+  }
 
-    return result.sort();
+  return result.sort();
 }
